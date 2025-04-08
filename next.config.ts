e7df703bacdea22ecac76b83702d98dba8e7c062
Daniel <danielakanbi01@gmail.com>
2025-04-08 14:00:27 +0100

    test

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Prevent pdfjs-dist from trying to load 'canvas' on the client
-      config.resolve.alias.canvas = false;
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
